2f3de3ef91569c610037c01cd3af19ad8852d5aa
Bonoshin <bonoshinpeace@gmail.com>
2025-02-20 19:55:12 +0200

    Task and Tasklist incomplete

---
+++ Task.py
@@ -0,0 +1,10 @@
+class Task:
+    def __init__(self):
+        self.complete = False
+        pass
+
+    def isComplete():
+        if self.complete == True:
+            return True
+        else:
+            return False
---
+++ Tasklist.py
@@ -0,0 +1,12 @@
+class Tasklist:
+    def __init__(self, size, tasklist):
+        self.size = 0
+        self.tasklist = []
+
+    
+
+    def Complete():
+        for i in range(self.size):
+            if tasklist[i].isComplete() == False:
+                return False
+            
