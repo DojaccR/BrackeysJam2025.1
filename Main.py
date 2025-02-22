69c337f5333d45d14dc4bf33d8f428371be82b58
ethanmwilliams <ethan393.williams@gmail.com>
2025-02-22 02:10:38 +0200

    Finished structure of Main. Need to add details of artwork

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import Maze
 from pygame.locals import *
 import asyncio
 import lock as lk
+import Game
+import time
 
 async def main():
     pygame.init()
@@ -10,30 +12,53 @@ async def main():
 
     ##TODO load resources
     mainMenu = pygame.image.load("main.png")
+    winningScreen = pygame.image.load("winscreen.png")
+    losingScreen = pygame.image.load("losescreen.png")
+
+    ## TODO get coordinates of buttons
+    startButton = pygame.Rect(x1, y1, x2, y2)
+    exitButton  = pygame.Rect(x1, y1, x2, y2)
+
+    clock = pygame.time.Clock()
 
     run = True
+    hasPlayed = False
 
     display = mainMenu
 
     while run:
-        ##TODO display current screen
         win.blit(display,(0, 0))
+
+        if hasPlayed:
+            time.sleep(5)
+            display = mainMenu
+            hasPlayed = False
 
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_position = pygame.mouse.get_pos()             # Location of the mouse-click
 
-                ##TODO On exit
+                if (exitButton.collidepoint(mouse_position)):    
+                    run = False
+                    pygame.quit()
+                    break
+
+                if (startButton.collidepoint(mouse_position)):    
+                    game = Game.Game(win)
+                    if (game.run()):
+                        display = winningScreen
+                    else:
+                        display = losingScreen
+                    hasPlayed = True
+
+            if event.type == QUIT:
                 run = False
                 pygame.quit()
+                break
 
-                ##TODO On start
-                    ## instantiate game
-                        ## if run false
-                            ## display losing screen
-                        ## if run true
-                            ## display winning screen
+        pygame.display.flip()    
+        clock.tick(60)
 
-                    ## display main menu
+    return
 
-        pass
-
+asyncio.run(main())
