33c01ffba79046b29760932fe7bb73d9ebbebc0e
Sanoshin <bonoshinpeace@gmail.com>
2025-02-23 12:27:01 +0200

    fixed stocks

--- Stocks.py
+++ Stocks.py
@@ -38,7 +38,6 @@ class Stocks(Task):
         big_win_font = pygame.font.SysFont(None, 72)
         loser_font = pygame.font.SysFont(None, 72)
     
-        fluctuationRange = 30
         clock = pygame.time.Clock()
 
         input_box = pygame.Rect(width // 2 + 50, height // 2 - 20, 140, 40)
@@ -48,39 +47,123 @@ class Stocks(Task):
         active = False
         text = ''
 
-        place_bet_text = "Place Bet"
-        place_bet_surface = font.render(place_bet_text, True, pygame.Color('white'))
+        place_bet_surface = font.render("Place Bet", True, pygame.Color('white'))
 
         while True:
             screen.fill((0, 0, 0))
-            
+        
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if input_box.collidepoint(event.pos):
+                        active = True
+                        color = color_active
+                    else:
+                        active = False
+                        color = color_inactive
+                if event.type == pygame.KEYDOWN:
+                    if active:
+                        if event.key == pygame.K_RETURN:
+                            if text.isdigit() and int(text) <= self.money:
+                                self.bet = int(text)
+                                self.money -= self.bet
+                                self.game_started = True
+                            text = ''
+                        elif event.key == pygame.K_BACKSPACE:
+                            text = text[:-1]
+                        else:
+                            text += event.unicode
 
-            win_condition_text = "Make $2000 to Win!"
-            win_condition_surface = font.render(win_condition_text, True, pygame.Color('yellow'))
-            screen.blit(win_condition_surface, (width // 2 + 50, height // 2-200))
-            
             screen.blit(place_bet_surface, (width // 2 + 50, height // 2 - 60))
+
             pygame.draw.rect(screen, color, input_box, 2)
             txt_surface = bet_font.render(text, True, pygame.Color('white'))
             screen.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
             pygame.draw.rect(screen, color, input_box, 2)
 
-            money_text = f"Money: ${self.money}"
-            money_surface = font.render(money_text, True, pygame.Color('white'))
+            money_surface = font.render(f"Money: ${self.money}", True, pygame.Color('white'))
             screen.blit(money_surface, (width // 2 + 50, height // 4))
 
-            bet_text = f"Bet: ${self.bet}"
-            bet_surface = font.render(bet_text, True, pygame.Color('white'))
+            bet_surface = font.render(f"Bet: ${self.bet}", True, pygame.Color('white'))
             screen.blit(bet_surface, (width // 2 + 50, height // 3))
 
-            
+            if self.money <= 0:
+                loser_surface = loser_font.render("LOSER!", True, pygame.Color('red'))
+                screen.blit(loser_surface, (width // 2 - loser_surface.get_width() // 2, height // 2))
+                pygame.display.flip()
+                pygame.time.wait(2000)
+                self.reset_game()
+                continue
+
+            if self.money >= 2000:
+                big_win_surface = big_win_font.render("Big Wins!", True, pygame.Color('green'))
+                screen.blit(big_win_surface, (width // 2 - big_win_surface.get_width() // 2, height // 2))
+                pygame.display.flip()
+                pygame.time.wait(2000)
+                pygame.quit()
+                sys.exit()
+
+            if self.game_started:
+                self.game_loop(screen, clock, width, height)
 
             pygame.display.flip()
             clock.tick(30)
+
+    def game_loop(self, screen, clock, width, height):
+        fluctuationRange = 10
+        while self.xPos < width // 2:
+            screen.fill((0, 0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            marketTrend = random.choice([1, -1])
+            fluctuation = random.randint(0, fluctuationRange) * marketTrend
+
+            self.needlePos += fluctuation
+            if self.needlePos < 0:
+                self.needlePos = 0
+            elif self.needlePos > height:
+                self.needlePos = height
+
+            self.previous_needle_pos = self.needlePos
+
+            xFluctuation = random.randint(1, 5)
+            self.xPos += xFluctuation
+
+            self.positions.append((self.xPos, self.needlePos))
+
+            for i in range(1, len(self.positions)):
+                pygame.draw.line(screen, (255, 0, 0), self.positions[i-1], self.positions[i], 2)
+
+            font = pygame.font.SysFont(None, 36)
+            money_surface = font.render(f"Money: ${self.money}", True, pygame.Color('white'))
+            screen.blit(money_surface, (width // 2 + 50, height // 4))
+
+            bet_surface = font.render(f"Bet: ${self.bet}", True, pygame.Color('white'))
+            screen.blit(bet_surface, (width // 2 + 50, height // 3))
+
+            pygame.display.flip()
+            clock.tick(30)
+
+        self.end_game()
+
+    def end_game(self):
+        movement = self.needlePos - 300
+    
+        if movement > 0:
+            self.money += self.bet * (1 + movement / 100)
+        elif movement < 0:
+            self.money -= self.bet
+
+        self.bet = 0
+        self.xPos = 0
+        self.needlePos = 300
+        self.positions = [(self.xPos, self.needlePos)]
+        self.game_started = False
 
     def reset_game(self):
         self.money = 1000
@@ -89,7 +172,6 @@ class Stocks(Task):
         self.needlePos = 300
         self.positions = [(self.xPos, self.needlePos)]
         self.game_started = False
-
 
 def main():
     test = Stocks("Stock Market", 1000)
